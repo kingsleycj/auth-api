a8968a87b7f5470f576872661001b9ee6f6b822d
kingsleycj <primekings.kc@gmail.com>
2025-04-18 18:03:34 +0100

    profile view implementation

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,7 +2,26 @@ const jwt = require("jsonwebtoken")
 function protect(req, res, next) {
     const authHeader = req.headers.authorisation;
 
+    // If missing or not in "Bearer <token>" format, reject
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided, Authorisation denied." })
+    }
+
+    // Extract the toke part
+    const token = authHeader.split(" ")[1]
+
+    try {
+        // Verify token and extract payload
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        // Attach user info for downstream handlers
+        req.user = { id: decoded.userId }
+        next();
+    } catch (err) {
+        console.log("Auth middleware error: ", err);
+        return res.status(401).json({ message: "Token Invalid or expired, authorisation denied. "})
         
     }
 }
+
+module.exports = protect;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router();
 const authController = require("../controllers/auth.controller")
+const profile = require("../controllers/profile.controller")
 
+// Public routes
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+
+// Protected route: fetch own profile
+router.get("/me", protect, profile.viewProfile)
+
 
 module.exports = router;
